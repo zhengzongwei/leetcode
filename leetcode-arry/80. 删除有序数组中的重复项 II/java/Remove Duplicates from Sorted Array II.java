d4c28f02d547bd17a724c3b79a6b3ae51b3f8c3e
tianhan <tianhan@tianhandeMacBook-Pro.local>
2021-05-08 20:56:09 +0800

    添加88题Java代码

---
+++ leetcode-arry/80. 删除有序数组中的重复项 II/java/Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,28 @@
+class Solution{
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int n : nums) {
+            if(i < 2 || n > nums[i - 2]) {
+                nums[i++] = n;
+            }
+        }
+        return i;
+    }
+
+    public int doublePointer(int[] nums) {
+        int left = 0;
+        int count = 0;
+        for(int right = 1;right < nums.length;right++) {
+            if(nums[left] != nums[right]) {
+                nums[++left] = nums[right];
+                count = 0;
+            } else if(count >= 1) {
+                continue;
+            } else {
+                nums[++left] = nums[right]
+                count++;
+            }
+        }
+        return left + 1;
+    }
+}
