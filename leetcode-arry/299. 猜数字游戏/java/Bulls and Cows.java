5e3ae96e6689239e73a4dc5e21d8fddd3b225e12
tianhan <tianhan@adsl-172-10-0-1.dsl.sndg02.sbcglobal.net>
2021-08-25 16:49:39 +0800

    添加 299 题,额外解答

--- leetcode-arry/299. 猜数字游戏/java/Bulls and Cows.java
+++ leetcode-arry/299. 猜数字游戏/java/Bulls and Cows.java
@@ -4,27 +4,41 @@ class Solution {
         int cows = 0;
 
         // PLAN 1
-        char[] sArr = secret.toCharArray();
-        char[] gArr = guess.toCharArray();
-
-        for (int i = 0; i < gArr.length; i++) {
-            if (gArr[i] == sArr[i]) {
-                bulls++;
-                sArr[i] = 'F';
-            } else {
-                for (int j = 0; j < sArr.length; j++) {
-                    if( gArr[i] == sArr[j] && sArr[j] != gArr[j] ) {
-                        cows++;
-                        sArr[j] = 'F';
-                        break;
-                    }
-                }
-            }
-            gArr[i] = 'F';
-        }
+//        char[] sArr = secret.toCharArray();
+//        char[] gArr = guess.toCharArray();
+//
+//        for (int i = 0; i < gArr.length; i++) {
+//            if (gArr[i] == sArr[i]) {
+//                bulls++;
+//                sArr[i] = 'F';
+//            } else {
+//                for (int j = 0; j < sArr.length; j++) {
+//                    if( gArr[i] == sArr[j] && sArr[j] != gArr[j] ) {
+//                        cows++;
+//                        sArr[j] = 'F';
+//                        break;
+//                    }
+//                }
+//            }
+//            gArr[i] = 'F';
+//        }
 
         // PLAN 2
+        int[] mnt = new int[10];
+        for (int i = 0; i < guess.length(); i++) {
+            if (guess.charAt(i) == secret.charAt(i)) {
+                bulls++;
+            } else {
+                if(++mnt[secret.charAt(i) - '0'] <= 0) {
+                    cows++;
+                }
+                if(--mnt[guess.charAt(i) - '0'] >= 0) {
+                    cows++;
+                }
+            }
+        }
 
-        return String.format("%sA%sB", bulls, cows);
+        StringBuilder build = new StringBuilder();
+        return build.append(bulls).append("A").append(cows).append("B").toString();
     }
 }
