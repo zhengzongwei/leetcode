f912372de3409328486d61369fb990666a288ecd
zhengzongwei <zhengzongwei@foxmail.com>
2021-08-28 22:01:59 +0800

    提交169题题解，需要优化超时限制

---
+++ leetcode-arry/169. 多数元素/python/Majority Element.py
@@ -0,0 +1,37 @@
+
+from typing import Counter, List
+
+
+def majorityElement(nums:List[int]) -> int:
+
+    count_num = len(nums) /2
+    total_count = 0
+    
+    for i in range(len(nums)):
+        index = nums[i]
+        count = 0
+        for j in range(i,len(nums)):
+            if index == nums[j]:
+                count +=1
+        if count > count_num:
+            if total_count !=0:
+                if count > total_count:
+
+                    total_count = count
+                    data = index
+            
+            total_count = count
+            data = index
+            
+
+
+
+    
+    return data
+
+print(majorityElement([3,3,4]))
+
+
+        
+        
+
