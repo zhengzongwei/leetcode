937c90db126a92369e1d962a2d997b5702ffa194
zhengzongwei <zhengzongwei@foxmail.com>
2021-08-28 22:02:26 +0800

    格式化代码

--- leetcode-arry/169. 多数元素/python/Majority Element.py
+++ leetcode-arry/169. 多数元素/python/Majority Element.py
@@ -2,36 +2,28 @@
 from typing import Counter, List
 
 
-def majorityElement(nums:List[int]) -> int:
+def majorityElement(nums: List[int]) -> int:
 
-    count_num = len(nums) /2
+    count_num = len(nums) / 2
     total_count = 0
-    
+
     for i in range(len(nums)):
         index = nums[i]
         count = 0
-        for j in range(i,len(nums)):
+        for j in range(i, len(nums)):
             if index == nums[j]:
-                count +=1
+                count += 1
         if count > count_num:
-            if total_count !=0:
+            if total_count != 0:
                 if count > total_count:
 
                     total_count = count
                     data = index
-            
+
             total_count = count
             data = index
-            
 
-
-
-    
     return data
 
-print(majorityElement([3,3,4]))
 
-
-        
-        
-
+print(majorityElement([3, 3, 4]))
