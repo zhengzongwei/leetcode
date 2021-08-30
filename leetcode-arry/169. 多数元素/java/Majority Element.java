95116ffc012c2ab158638d9f12e3d1f934119265
tianhan <nieat@foxmail.com>
2021-08-30 09:14:26 +0800

    优化169题，代码

--- leetcode-arry/169. 多数元素/java/Majority Element.java
+++ leetcode-arry/169. 多数元素/java/Majority Element.java
@@ -1,18 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = Math.floorDiv(nums.length, 2);
         Map<Integer, Integer> hash = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             hash.put(nums[i], hash.getOrDefault(nums[i], 0) + 1);
         }
-        int val = count;
-        int key = 0;
+        int count = 0;
+        int element = 0;
         for (Integer k : hash.keySet()) {
-            if (val < hash.get(k)) {
-                val = hash.get(k);
-                key = k;
+            if (count < hash.get(k)) {
+                count = hash.get(k);
+                element = k;
             }
         }
-        return key;
+        return element;
     }
+}
+
+class Solution2 {
+
 }
