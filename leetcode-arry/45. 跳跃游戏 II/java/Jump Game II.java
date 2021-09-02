2a257fac1a6552b5ed5022445db6220a88d3241f
tianhan <nieat@foxmail.com>
2021-09-02 15:20:01 +0800

    添加 45 题

---
+++ leetcode-arry/45. 跳跃游戏 II/java/Jump Game II.java
@@ -0,0 +1,18 @@
+class Solution {
+    // [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]
+    // 参考贪心算法思路，遍历寻找最优解
+    public int jump(int[] nums) {
+        int len = nums.length;
+        int jump = len == 1 ? 0 : 1;
+        int farthest = nums[0];
+        for (int i = 1; i < len && farthest < len - 1; i++) {
+            int possibleMostFarthestJump = 0;
+            for (int j = i; j <= farthest; j++) {
+                possibleMostFarthestJump = Math.max(possibleMostFarthestJump, nums[j] + j);
+            }
+            jump++;
+            farthest = possibleMostFarthestJump;
+        }
+        return jump;
+    }
+}
