da20a79c043410183f85fc3b3922d931cfe1a13d
tianhan <nieat@foxmail.com>
2021-09-02 14:14:38 +0800

    55 题 添加动态规划解法

--- leetcode-arry/55. 跳跃游戏/java/Jump Game.java
+++ leetcode-arry/55. 跳跃游戏/java/Jump Game.java
@@ -17,7 +17,18 @@ class Solution {
 }
 
 class Solution2 {
+    // 动态规划
     public boolean canJump(int[] nums) {
-        return false;
+        int len = nums.length;
+        boolean[] db = new boolean[len];
+        db[0] = true;
+        for (int i = 0; i < len; i++) {
+            if (db[i]) {
+                for (int j = 1; j <= nums[i] && i + j < len; j++) {
+                    db[i + j] = true;
+                }
+            }
+        }
+        return db[len - 1];
     }
 }
