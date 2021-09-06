f0ca8865e724ca5133d558c3b629085510b0b85f
tianhan <nieat@foxmail.com>
2021-09-06 15:14:18 +0800

    添加 220 题

---
+++ leetcode-arry/220. 存在重复元素 III/java/Contains Duplicate III.java
@@ -0,0 +1,17 @@
+class Solution {
+    // 滑动窗口 + TreeSet集合
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        int len = nums.length;
+        TreeSet<Long> tree = new TreeSet<>();
+        for (int i = 0; i < len; i++) {
+            // ceiling() the least key greater than or equal to key, or null if there is no such key
+            Long ceiling = tree.ceiling((long) nums[i] - (long) t);
+            if (ceiling != null && Math.abs((long) nums[i] - ceiling) <= t) return true;
+            tree.add((long) nums[i]);
+            if (tree.size() > k) {
+                tree.remove((long) nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
