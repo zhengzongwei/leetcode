48e082592ac9d74e5228e6c212b68c43f9ceb33e
zhengzongwei <zhengzongwei@foxmail.com>
2021-09-08 17:17:49 +0800

    提交217的第二种解法

--- leetcode-arry/217. 存在重复元素/python/Contains Duplicate.py
+++ leetcode-arry/217. 存在重复元素/python/Contains Duplicate.py
@@ -1,6 +1,14 @@
 def containsDuplicate(nums)->bool:
     ret = False
-    length = len(nums)
-    if length != len(set(nums)):
-        ret = True
+    # 解法一 直接利用集合去重的特性比对数组长度
+    # ret = False
+    # length = len(nums)
+    # if length != len(set(nums)):
+    #     ret = True
+
+    # 解法二 排序比较相邻的两个元素是否相等
+    nums.sort()
+    for i in range(len(nums)-1):
+        if nums[i]== nums[i+1]:
+            ret = True
     return ret
