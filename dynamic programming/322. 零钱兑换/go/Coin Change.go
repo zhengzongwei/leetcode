c00c9f6bc5db9c093446ee5b8e4ce1430e1ca90e
tianhan <nieat@foxmail.com>
2021-09-24 14:54:21 +0800

    322 题 添加Go语言

--- dynamic programming/322. 零钱兑换/go/Coin Change.go
+++ dynamic programming/322. 零钱兑换/go/Coin Change.go
@@ -10,24 +10,21 @@ func coinChange(coins []int, amount int) int {
 	if amount == 0 {
 		return 0
 	}
-
 	dp := make([]int, amount+1)
 	sort.Ints(coins)
 	for i := 1; i < len(dp); i++ {
 		min := 1 << 32
-
 		for c := 0; c < len(coins) && coins[c] <= i; c++ {
 			val := dp[i-coins[c]] + 1
 			if val > 0 && val < min {
 				min = val
 			}
 		}
-
 		if min == 1 << 32 {
 			dp[i] = -1
-		} else {
-			dp[i] = min
+			continue
 		}
+		dp[i] = min
 	}
 	return dp[amount]
 }
