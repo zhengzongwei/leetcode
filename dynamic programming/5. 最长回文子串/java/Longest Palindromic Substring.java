903b37c3197b883c4b2537af75aa9e4ad43fbb10
tianhan <nieat@foxmail.com>
2021-12-09 16:30:08 +0800

    提交 5. 最长回文子串 题

---
+++ dynamic programming/5. 最长回文子串/java/Longest Palindromic Substring.java
@@ -0,0 +1,34 @@
+class Solution {
+    // 动态规划，还存在优化的空间
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        String[] dp = new String[len];
+        String[] origin = s.split("");
+
+        dp[0] = origin[0];
+        for (int i = 1; i < len; i++) {
+            String t = "";
+            for (int j = 0; j <= i; j++) {
+                if (origin[j].equals(origin[i])) {
+                    Boolean isLevel = isStrLevel(origin, j, i);
+                    if (isLevel) {
+                        t = s.substring(j, i + 1);
+                        break;
+                    }
+                }
+            }
+            dp[i] = t.length() > dp[i - 1].length() ? t : dp[i - 1];
+        }
+
+        return dp[len - 1];
+    }
+
+    public static Boolean isStrLevel(String[] arr, int l, int r) {
+        for (; l < r; l++, r--) {
+            if (!arr[l].equals(arr[r])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
