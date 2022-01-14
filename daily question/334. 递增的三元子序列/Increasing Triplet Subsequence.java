bd444d9b83dd666ea8ac074a59aa6a1b90a2e6c6
tianhan <nieat@foxmail.com>
2022-01-14 10:13:07 +0800

    修改334解题

--- daily question/334. 递增的三元子序列/Increasing Triplet Subsequence.java
+++ daily question/334. 递增的三元子序列/Increasing Triplet Subsequence.java
@@ -18,4 +18,24 @@ class Solution {
         }
         return false;
     }
+
+    /**
+     * 超时时间复杂度O(n^2)
+     * @param nums
+     * @return boolean
+     */
+    public boolean abandon(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int v1 = Integer.MAX_VALUE;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > nums[i]) {
+                    v1 = Math.min(v1, nums[j]);
+                }
+                if (nums[j] > v1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
